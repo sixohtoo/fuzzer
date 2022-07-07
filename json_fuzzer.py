b40c99d11dc8f380070659600b92e23bf85caae2
Elliot Rotenstein <elliot@balgara.com>
2022-07-07 14:05:05 +1000

    everything results in valid json

--- json_fuzzer.py
+++ json_fuzzer.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 import json
 import random
-from utils import *
+import utils as u
+import time
+from math import pi
 
 MAX_INT = 2147483647
 MIN_INT = -2147483648
 
+
 def fuzz_json(prog_name, input_name):
     with open(input_name, "r") as f:
         data_raw = f.read()
-    for _ in range(10):
+    option = 0
+    while True:
         data = json.loads(data_raw)
+        field = u.get_random_field(data)
+        if option == 0:
+            add_field(data)
+        elif option == 1:
+            remove_field(data, field)
+        elif option == 2:
+            flip_bits(data, field)
+        elif option == 3:
+            swap_type(data, field)
+        elif option == 4:
+            smart_swap(data, field)
+            option = -1
+        
+        option += 1
+        print(json.dumps(data, indent=2))
+        # time.sleep(2)
 
     
 
@@ -18,24 +38,59 @@ def add_field(data):
     data['admin'] = 'something (may change this later)'
     data['another'] = 602.602
 
-def remove_field(data):
-    del data[get_random_field(data)]
+def remove_field(data, field):
+    print('removing', field)
+    del data[field]
 
-def flip_bits(data):
-    pass
+# ''.join(format(i, '08b') for i in bytearray(test_str, encoding ='utf-8'))
+# ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)]*8))
+def flip_bits(data, field):
+    current = data[field]
+    print('flipping', field)
+    if isinstance(current, int):
+        bits = bin(current)
+        data[field] = int(u.flip_bits(bits[2:]), 2)
+    elif isinstance(current, str):
+        bits = u.str_to_bits(current)
+        data[field] = u.bits_to_str(u.flip_bits(bits))
 
-def swap_type(data):
+# make this recursive somehow (lists inside of dicts inside of lists, etc)
+# Maybe add function as a data type?
+def swap_type(data, field):
     '''
-    types: int, string, list, dictionary, nothing
+    types: int, string, list, empty list, dict, empty dictionary, bool
     '''
-    pass
+    print('swapping', field)
+    option = random.randrange(0, 8)
+    print("chose option", option)
+    current = data[field]
+    if option == 0: # int
+        data[field] = 3
+    elif option == 1: # string
+        data[field] = "hello world"
+    elif option == 3:
+        data[field] = [1, 2, "hello"]
+    elif option == 4:
+        data[field] = []
+    elif option == 5:
+        data[field] = {
+            'a' : 'hello',
+            1 : option % 3,
+            True : False,
+            None : ['a', 3, None],
+            3.1 : {'a' : 4}
+        }
+    elif option == 6:
+        data[field] = {}
+    elif option == 7:
+        data[field] = True
 
-def smart_swap(data):
-    field = get_random_field(data)
+def smart_swap(data, field):
+    print('smart swapping', field)
     current = data[field]
     if isinstance(current, int):
         smart_swap_int(data, field)
-    elif isintance(current, str):
+    elif isinstance(current, str):
         smart_swap_string(data, field)
 
 # Fields expects a number, so try
@@ -43,6 +98,8 @@ def smart_swap(data):
 def smart_swap_int(data, field):
     option = random.randrange(0, 10)
     current = data[field]
+    print('smart string', field)
+    print('chose option', option)
     if option == 0:
         data[field] = int(current)
     elif option == 1:
@@ -56,7 +113,7 @@ def smart_swap_int(data, field):
     elif option == 5:
         data[field] = MIN_INT
     elif option == 6:
-        data[field] = math.PI
+        data[field] = pi
     elif option == 7:
         data[field] = MAX_INT + 1
     elif option == 8:
@@ -67,31 +124,37 @@ def smart_swap_int(data, field):
 # Field expects a string, so try
 # normal string, large string, byte string, add random \s, add ", " and `, empty string
 def smart_swap_string(data, field):
-    option = random.randrange(0, 5)
+    option = random.randrange(0, 6)
     current = data[field]
     length = random.randrange(1, 10000)
+    print('smart string', field)
+    print('chose option', option)
     if option == 0:
         data[field] = str(current)
     elif option == 1:
-        data[field] = generate_string(length)
+        data[field] = u.generate_string(length)
     elif option == 2:
-        data[field] = generate_bytes(length)
-    elif option == 4:
+        data[field] = str(u.generate_bytes(length))
+    elif option == 3:
         # Adds a \ every 3rd character
         for i in range(len(current)):
             if i % 3 == 0:
-                current = insert_into_string(current, i, '\\')
+                current = u.insert_into_string(current, i, '\\')
         data[field] = current
-    elif option == 5:
+    elif option == 4:
         # Adds a quote every 3rd character
         chars = ['\'', '\"', '`']
         for i in range(len(current)):
             if i % 3 == 0:
-                current = insert_into_string(current, i, random.choice(chars))
+                current = u.insert_into_string(current, i, random.choice(chars))
         data[field] = current
-    elif option == 6:
+    elif option == 5:
         data[field] = ""
 
 
 if __name__ == '__main__':
     fuzz_json("a", "bin/json1.txt")
+    # a = 'hello'
+    # a = u.generate_bytes(10)
+    # print(a)
+    # print(str(a))
--- utils.py
+++ utils.py
@@ -19,12 +19,45 @@ def generate_string(length):
 
 # Returns name of a random field inside data dictionary
 def get_random_field(data):
-	return data.keys()[random.randrange(0, len(data))]
+	fields = [x for x in data]
+	return fields[random.randrange(0, len(data))]
 
 def insert_into_string(string, index, char):
 	return string[:index] + char + string[index:]
 
+def flip_bits(bits):
+	# print("flipping", bits)
+	# print("len", len(bits))
+	arr = [x for x in bits]
+
+	freq = random.randrange(1, len(bits) + 1)
+	# print("freq", freq)
+	for i in range(len(arr)):
+		if (i + 1) % freq == 0:
+			# print('e')
+			arr[i] = '0' if int(arr[i]) else '1'
+	return ''.join(arr)
+	# arr = bits.split(" ")
+	# print(arr)
+
+def str_to_bits(text):
+	return ''.join(format(i, '08b') for i in bytearray(text, encoding ='utf-8'))
+
+def bits_to_str(bits):
+	return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(bits)]*8))
+
 if __name__ == '__main__':
+	x = 5
+	bits = bin(x)
+	flipped = flip_bits(bits[2:])
+	print(flipped)
+	print(int(bits, 2))
+	print(int(flipped, 2))
+	
 	# for i in range(100):
 		# print(generate_plaintext(i))
-	generate_bytes()
+	# s = 'hello'
+	# bits = str_to_bits(s)
+	# flipped = flip_bits(bits)
+	# print(bits_to_str(bits))
+	# print(bits_to_str(flipped))
