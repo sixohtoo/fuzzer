43b97de5eb0230582a7d469c429af4992caacd85
AleksandraKal <z5312702@ad.unsw.edu.au>
2022-07-15 22:57:56 +1000

    code structure changes

--- csvFuzzer.py
+++ csvFuzzer.py
@@ -11,27 +11,54 @@ from pwn import *
 sampleInput = "bin/csv1.txt"
 sampleInput2 = "bin/csv2.txt"
 
-def main():
 
-    endTime = time.time() + 180
-    while time.time() < endTime:
-        io = process("bin/csv1")
-        # loop through the list!
-        sendDataToPwnTwls(modifiedData(sampleInput),io)
-        sendDataToPwnTwls(addLines(sampleInput),io)
-        # sendDataToPwnTwls(modifiedDelimiter(sampleInput),io)   
+def fuzz_csv(program, sampleInputFile):
+    sampleInput = convertCsvToList(sampleInputFile)
+    option = randomAsciiFromItem(list(range(1,10000))) % 3
+    print(option)
+    if option == 0:
+        dataToSend = addLines(sampleInput)
+    elif option == 1:
+        dataToSend = modifiedData(sampleInput)
+    elif option == 2:
+        # send in nothing to program, ie simulate CTRL-D
+        dataToSend = None
+   
+    io = process(program)
+    if (dataToSend != None):
+        sendDataToPwnTwls(dataToSend,io)
+    io.proc.stdin.close()
+    exitCode = io.poll(block=True)
+    if (exitCode != 0):
+        print(f"The data that causes the program to crash is : {dataToSend}")
 
-        io2 = process("bin/csv2")
-        sendDataToPwnTwls(modifiedData(sampleInput2),io2)
-        sendDataToPwnTwls(addLines(sampleInput2),io2)
-        #sendDataToPwnTwls(modifiedDelimiter(sampleInput2),io2)   
+    
+    # endTime = time.time() + 180
+    # while time.time() < endTime:
+    # io = process("bin/csv1")
+    # # loop through the list!
+    # #sendDataToPwnTwls(addLines(sampleInput),io)
+    # #sendDataToPwnTwls(modifiedData(sampleInput),io)
+    # #sendDataToPwnTwls(modifiedDelimiter(sampleInput),io)
+    # #sendEmptyToPwnTwls(io)
+    # io.proc.stdin.close()
+    # exitCode = io.poll(block=True)
+    # print(exitCode)
+
+    # # #csv2 
+    # io2 = process("bin/csv2")
+    # #sendDataToPwnTwls(modifiedData(sampleInput2),io2)
+    # #sendDataToPwnTwls(addLines(sampleInput2),io2)
+    # #sendDataToPwnTwls(modifiedDelimiter(sampleInput2),io2)  
+    # #sendEmptyToPwnTwls(io2)
+    # io2.proc.stdin.close()
+    # exitCode2 = io2.poll(block=True)
+    # print(exitCode2) 
 
 
 # given a iterable list, send in each line of the list to a process in pwntools
 def sendDataToPwnTwls(inputList,process):
-    print(len(inputList),inputList)
     for line in inputList:
-        print(line)
         process.sendline(line.encode())
 
 # send CTRL-D to proram
@@ -75,7 +102,7 @@ def convert2DList(list,delimiter):
                 newLine = word
         retList.append(newLine)
     return retList
-
+# ----------------------------------------- MUTATION FUNCTIONS -------------------------------------------- #
 # TODO : return a modified version of the header in sampleInput
 # def manipulateCSVheader(sampleInput):
 #     modifiedHeader = []
@@ -97,7 +124,7 @@ def convert2DList(list,delimiter):
 
 selection = ["%s","%n","A"*99, "%n"]
 def modifiedData(sampleInput):
-    modifiedData = convertCsvToList(sampleInput)
+    modifiedData = sampleInput
     for i,row in enumerate(modifiedData):
         # changing the ascii characters of the input
             newRow = []
@@ -112,18 +139,18 @@ def modifiedData(sampleInput):
 # TODO : fix eof in pwntools bug
 def modifiedDelimiter(sampleInput):
     modifiedDelimiter = ["\n","","%","%n","%s","%d",randomAsciiGen()]
-    return convert2DList(convertCsvToList(sampleInput), random.choice(modifiedDelimiter))
+    return convert2DList(sampleInput, random.choice(modifiedDelimiter))
 
 # add large amount of rows to the csvFile
 def addLines(sampleInput):
-    data = convertCsvToList(sampleInput)
+    data = sampleInput
     rowSelection= ["%n","%d","%s","%p","%x"]
     rowToAdd = []
     # make a new row, with number of elements as large as the width
     for i in range(0,len(data[0])):
         rowToAdd.append(randomAsciiFromItem(rowSelection))
     # choose the number of times to loop over and add row of data
-    selection = [40,50,60,70,80,90,100]
+    selection = [50,100,150,200,250,300,350]
     for i in range(randomAsciiFromItem(selection)):
         data.append(rowToAdd)
     return convert2DList(data,',')
@@ -139,4 +166,5 @@ def emptyCsv():
 
 
 if __name__ == "__main__":
-    main()
+    fuzz_csv("bin/csv1","bin/csv1.txt")
+    fuzz_csv("bin/csv2","bin/csv2.txt")
