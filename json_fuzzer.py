50188898e3684a5f001c127a2e3739fbcb7478cd
wengxinn <wengxinn@outlook.com>
2022-08-07 17:35:19 +1000

    Added harness to csv and json

--- json_fuzzer.py
+++ json_fuzzer.py
@@ -40,11 +40,17 @@ def fuzz_json(prog_name, text, lock, option):
         final = mutate_raw_string(text)
     
     payload = final
-    p = process(prog_name, level='critical')
+    p = process(prog_name, level='critical', timeout=1.5)
     p.sendline(payload.encode('utf-8'))
 
     p.proc.stdin.close()
-    if p.poll(True) == -11:
+    exit_code = p.poll(True)
+    # Return if detected hangs/infinite loops
+    if exit_code is None:
+        print("Detected hangs/infinite loops. Program terminated")
+        return 
+
+    if exit_code == -11:
         with lock:
             with open("bad.txt", "w") as f:
                 f.write(final)
